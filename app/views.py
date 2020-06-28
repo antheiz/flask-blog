30f24df276189627e0b1909a7796b349f87d0e85
antroytheiz <salupacode@gmail.com.(none)>
2020-06-28 14:56:15 +0700

    add templates #1

--- app/views.py
+++ app/views.py
@@ -1,5 +1,25 @@
 from app import app
+from flask import render_template
+
+posts = [
+    {
+        'penulis':'Theis',
+        'judul':'postingan pertama',
+        'isi':'Ini postingan pertama',
+        'date_post':'Juni 28, 2020',
+    },
+    {
+        'penulis':'Jhon',
+        'judul':'postingan kedua',
+        'isi':'Ini postingan kedua',
+        'date_post':'Juni 28, 2020',
+    }
+]
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', judul='About')
