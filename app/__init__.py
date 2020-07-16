7fa68fece7654db5ec94e4919235416d33be4080
antroytheiz <salupacode@gmail.com.(none)>
2020-07-16 21:23:56 +0700

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,15 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.secret_key = 'ioafhwa97e9032iakdnwi'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://theis:salupa@localhost/flaskblog'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskblog.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://theis:salupa@localhost/flaskblog'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'danger'
+
 from app import routes
 
 
