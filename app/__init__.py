61bf227570a618420238367c0dbb6c316fef57fd
Theiz <45439030+antroytheiz@users.noreply.github.com>
2020-07-20 20:08:09 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,6 @@ from flask_login import LoginManager
 from flask_ckeditor import CKEditor
 from flask_mail import Mail
 
-
-
 app = Flask(__name__)
 app.secret_key = 'ioafhwa97e9032iakdnwi'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskblog.db'
@@ -25,7 +23,6 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'email'
 app.config['MAIL_PASSWORD'] = 'password'
-
 mail = Mail(app)
 
 from app import routes
